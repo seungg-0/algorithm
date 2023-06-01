2c3891deb578c5dfd126e4a2fc0e36909004b4b7
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-06-01 23:40:51 +0900

    Update 다리를 지나는 트럭.py

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -10,6 +10,8 @@ def solution(bridge_length, weight, truck_weights):
     truck.extend(truck_weights)
     bridge.extend([0 for _ in range(bridge_length)])
     
+    # bridge에 0을 넣거나 트럭을 보내거나 (건너지 않은 트럭이 있는 경우)
+    # bridge에 아무것도 보내지 않는다 (건너지 않은 트억이 없는 경우)
     while len(bridge) > 0:
         if len(truck) >= 1:
             box = truck.popleft()
