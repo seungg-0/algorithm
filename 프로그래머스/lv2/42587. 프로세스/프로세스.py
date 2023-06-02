64e1b91bb3e1370cab835259252f458c9533dd9c
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-06-02 18:49:36 +0900

    Update 프로세스.py

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,8 +1,6 @@
 from collections import deque
 # 숫자 클수록 우선순위 높음
 
-# 큐에서 하나 꺼내기 popleft
-# 큐에 남은 값들과 비교 - 우선순위 높은거 있다면 append / 아니라면 
 def solution(priorities, location):
     answer = 0
     # 중요도, 위치
@@ -12,7 +10,7 @@ def solution(priorities, location):
     for idx, val in enumerate(priorities):
         priorities_dict[idx] = val
         process.append(idx)
-    print(process)
+        
     while True:
         flag = False
         tmp = process.popleft()
@@ -22,9 +20,8 @@ def solution(priorities, location):
         if flag:
             process.append(tmp)
         else:
-            print(tmp, location, process, answer)
             answer += 1
             if tmp == location:
                 break
-
+                
     return answer
