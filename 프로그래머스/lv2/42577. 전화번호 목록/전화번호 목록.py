3d3ee8b53357aa1c4e0821eee3b74eba045f6465
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-06-07 17:40:05 +0900

    Update 전화번호 목록.py

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,7 @@
 def solution(phone_book):
     answer = True
     
+    # 실수한 부분 : for문을 두 번 돌릴 필요 없다 (효율성 생각하면서 코드 짜기)
     phone_book.sort()
     for i in range(1, len(phone_book)):
         if len(phone_book[i-1]) <= len(phone_book[i]):
