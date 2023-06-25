6629b90068b1de70135cdae446089a5d7cec76de
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-06-25 16:20:26 +0900

    [level 2] Title: 더 맵게, Time: 1874.51 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,19 +1,13 @@
-# heapq 자료형 활용하기
-# heapq.heappush(heap, item) : item을 heap에 추가
-# heapq.heappush(heap, 50)
-# heapq.heappop(heap) : heap에서 가장 작은 원소를 pop & 리턴. 비어 있는 경우 IndexError가 호출됨. 
-# result = heapq.heappop(heap)
-# heapq.heapify(x) : 리스트 x를 즉각적으로 heap으로 변환함 (in linear time, O(N) )
-import heapq
+from heapq import heappush, heappop, heapify
 
 def solution(scoville, K):
     answer = 0
-    heapq.heapify(scoville)
+    heapify(scoville)
 
-    while scoville[0] < K: # 가장 작은 값이 K보다 작으면
-        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville)*2) # 섞기
+    while scoville[0] < K: # 가장 작은 값이 K보다 커지기 전까지 반복
+        heappush(scoville, heappop(scoville) + heappop(scoville)*2) # 섞기
         answer += 1
-        if len(scoville) == 1 and scoville[0] < K: # 계속 섞으면 마지막은 1, 마지막이 K보다 작으면
+        if len(scoville) == 1 and scoville[0] < K: # 계속 섞으면 마지막은 1, 마지막이 K보다 작으면 -1
             return -1
-
+        
     return answer
