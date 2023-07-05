eb3146493182850889e8c872d9b656ff6ed0567a
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-07-05 18:16:26 +0900

    [level 2] Title: 전화번호 목록, Time: 110.05 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,11 @@
 def solution(phone_book):
     answer = True
     
-    # 실수한 부분 : for문을 두 번 돌릴 필요 없다 (효율성 생각하면서 코드 짜기)
     phone_book.sort()
     for i in range(1, len(phone_book)):
         if len(phone_book[i-1]) <= len(phone_book[i]):
                 if phone_book[i-1] == phone_book[i][:len(phone_book[i-1])]:
                     answer = False
                     break
-        
+    
     return answer
