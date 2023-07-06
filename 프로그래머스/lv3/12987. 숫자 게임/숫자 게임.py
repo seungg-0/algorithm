0dab501db7d54526cc3c20fed8960da2630dcdad
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-07-06 19:08:32 +0900

    Update 숫자 게임.py

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -1,16 +1,14 @@
+# B가 가진 숫자 중 큰 수부터 사용
+# B의 가장 큰 수랑 A의 가장 큰 수를 비교했을 때 A가 크거나 같으면 A의 두번째 큰 수와 B의 가장 큰 수 비교
+
 def solution(A, B):
     answer = 0
-    
     A.sort(reverse = True)
     B.sort(reverse = True)
     B_start = 0
-    for i in range(len(A)): # 0 1 2 3
+    for i in range(len(A)):
         if A[i] >= B[i-B_start]:
-            # print(A[i], B[i-B_start])
             B_start += 1
-        elif A[i] < B[i-B_start]:
-            # print(A[i], B[i])
+        else:
             answer += 1
-    
-    
     return answer
