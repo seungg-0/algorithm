0429a826959570ae7261f2dcd9a128e527f324d2
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-07-15 17:27:40 +0900

    [level 3] Title: 야근 지수, Time: 518.89 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
@@ -1,31 +1,39 @@
-# (전체 일을 마치는데 걸리는 시간) - n : 총 야근 시간
-# 최대한 많이 쪼갤수록 제곱했을 때 작은 값이 나옴
-# 총 야근 시간 / 일의 갯수 -> 나누어 떨어지지 않는 경우는 나머지 버리고 몫만 구한 값을 분자에서 빼주고 분모 -1
-
 def solution(n, works):
     answer = 0
+    # key : 업무량, value : 해당 업무량을 가진 일의 갯수
     work_dict = {}
     for work in works:
+        # key값이 이미 있으면
         if work_dict.get(work):
             work_dict[work] = work_dict[work] + 1
+        # key값이 없으면
         else:
             work_dict[work] = 1
+
+    # keys : 업무별 일해야 하는 시간에서 중복 제거한 값들
     keys = list(work_dict.keys())
     keys.sort()
     while n > 0:
         max_key = keys[-1]
-        work_dict[max_key] = work_dict[max_key] - 1
+        # 가장 큰 업무 1시간 처리
+        work_dict[max_key] -= 1
+        # 가장 큰 업무가 유일한 가장 큰 업무였다면 keys에서 제거
         if work_dict[max_key] == 0:
             keys.pop()
+        # 1시간 업무 처리 후 해당 업무량을 가진 업무가 또 있다면 해당 업무량의 일의 갯수 + 1
         if work_dict.get(max_key-1):
-            work_dict[max_key-1] = work_dict[max_key-1] + 1
+            work_dict[max_key-1] += 1
+        # 1시간 업무 처리 후 해당 업무량을 가진 업무가 또 있다면 새로 넣어주기
         elif max_key > 1:
             keys.append(max_key-1)
             keys.sort()
             work_dict[max_key-1] = 1
+        # 업무를 다끝냈다면 종료
         if len(keys) == 0:
             break
-        n = n - 1
+        n -= 1
+
     for key in keys:
+        # work_dict[key] : 업무 갯수
         answer += key * key * work_dict[key]
     return answer
