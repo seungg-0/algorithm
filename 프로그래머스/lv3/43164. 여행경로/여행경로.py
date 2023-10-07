2d8eebc0b1fd1187a95526150da675c377ba9882
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-07 19:10:39 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,20 +1,20 @@
-# ICN 출발
-# 복습 해야함 (이해 덜함)
 def solution(tickets):
     answer = []
     tickets.sort()
-    visited = [False for _ in range(len(tickets))]
+    usable_ticket = [True]*len(tickets)
     
-    def dfs(start, route):
-        if len(route) == (len(tickets)+1):
-            answer.append(route)
+    def dfs(now_airport, journey):
+        
+        # 종료 조건
+        if len(journey) == (len(tickets)+1):
+            answer.append(journey)
             return
+            
         for idx, ticket in enumerate(tickets):
-            if not visited[idx] and start == ticket[0]:
-                visited[idx] = True
-                dfs(ticket[1], route+[ticket[1]])
-                visited[idx] = False
+            if usable_ticket[idx] and ticket[0] == now_airport:
+                usable_ticket[idx] = False # 티켓 사용 처리
+                dfs(ticket[1], journey+[ticket[1]])
+                usable_ticket[idx] = True
     
-    dfs("ICN", ["ICN"])
-    answer = answer[0]
-    return answer
+    dfs('ICN', ['ICN'])
+    return answer[0]
