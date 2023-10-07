4adea3742220ccb6d795eae12b1aa898a6588f5e
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-08 00:25:19 +0900

    Update 단어 변환.py

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,66 +1,28 @@
-# # 한 번에 한 단어 변환
-# # words에 있는 단어로만 변환
-
-# def solution(begin, target, words):
-#     answer = []
-#     visited = [False]*len(words)
-    
-#     def dfs(word, change_cnt):
-        
-#         # 종료 조건 (target으로 바뀌면)
-#         if word == target:
-#             answer.append(change_cnt)
-#             return
-        
-#         else:
-#             for idx, change_word in enumerate(words):
-#                 # if not visited[idx]:
-#                 cnt = 0
-#                 for i in range(len(word)):
-#                     if word[i] != change_word[i]:
-#                         cnt += 1
-#                 if cnt == 1 and not visited[idx]:
-#                     visited[idx] = True
-#                     # tmp = word
-#                     # word = change_word
-#                     dfs(change_word, change_cnt+1)
-#                     # word = tmp
-#                     visited[idx] = False
-
-
-#     dfs(begin, 0) 
-#     answer.sort()
-#     if len(answer) > 1:
-#         return answer[0]
-#     else:
-#         return 0
-
-
-
+# DFS 풀이
+# 일단, 이 문제와 같이 "최단거리"를 구하는 문제는 BFS로 푸는게 더 간편하다 ! 항상 뭘 쓰는게 더 좋을지 생각 먼저 하기.
+# 처음 정답률 20%로 좀 시간이 걸렸는데, if 부분에서 return을 해주는게 아니라 그냥 answer값을 업데이트 해주면 됐었다.
 
 def solution(begin, target, words):
-    answer = 24700000000
-    flag = 0
-    m = len(words)
-    ch = [0] * m
-    def dfs(word,target,val):
-        nonlocal flag, m, answer
+    answer = 1e9
+    visited = [False]*len(words)
+    
+    def dfs(word, change_cnt):
+        nonlocal answer
         if word == target:
-            flag = 1
-            answer = min(answer,val)
-        else:
-            for i in range(m):
-                cnt = 0
-                #한 번에 한 개의 알파벳만 바꿀 수 있는 조건 체크
-                for j in range(len(word)):
-                    if word[j] != words[i][j]: 
-                        cnt += 1
-                if ch[i] == False and cnt == 1:
-                    ch[i] = True
-                    dfs(words[i],target,val + 1)
-                    ch[i] = False
-    dfs(begin,target,0)
-    if flag:
-        return answer
-    else:
+            answer = min(answer, change_cnt)
+        
+        for idx, change_word in enumerate(words):
+            cnt = 0
+            for i in range(len(word)):
+                if word[i] != change_word[i]:
+                    cnt += 1
+            if cnt == 1 and not visited[idx]:
+                visited[idx] = True
+                dfs(change_word, change_cnt+1)
+                visited[idx] = False
+
+    dfs(begin, 0)
+    if answer == 1e9:
         return 0
+    else:
+        return answer
