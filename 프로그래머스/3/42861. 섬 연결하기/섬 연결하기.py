ba0be3c86be2857f794046c8417efa0ffe319152
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-14 18:15:36 +0900

    Update 섬 연결하기.py

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,33 +1,14 @@
-# def solution(n, costs):
-#     answer = 0
-#     costs.sort(key=lambda x:x[2]) # 가격 기준 오름차순 정렬
-#     link = set([costs[0][0]])
-    
-#     while len(link) < n:
-#         for node1, node2, cost in costs:
-#             if node1 in link and node2 in link:
-#                 continue
-#             if node1 in link or link2 in link:
-#                 link.update([node1], [node2])
-#                 answer += cost
-#                 break
-#     return answer
-
 def solution(n, costs):
     answer = 0
-    costs.sort(key = lambda x: x[2]) 
+    costs.sort(key=lambda x:x[2]) # 가격 기준 오름차순 정렬
     link = set([costs[0][0]])
-
-    # Kruskal 알고리즘으로 최소 비용 구하기
+    
     while len(link) < n:
-        for v in costs:
-            # print(v, v[0], v[1], v[2])
-            if v[0] in link and v[1] in link:
+        for node1, node2, cost in costs:
+            if node1 in link and node2 in link: # 둘 다 이미 연결되어 있다면
                 continue
-            if v[0] in link or v[1] in link:
-                link.update([v[0], v[1]])
-                answer += v[2]
+            if node1 in link or node2 in link: # 둘 중 하나는 연결되어 있지 않고 하나는 연결되어 있다면
+                link.update([node1], [node2])
+                answer += cost
                 break
-                
     return answer
-
