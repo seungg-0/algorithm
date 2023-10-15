209064bd2d787d228f5749ea274e288ed7b28485
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-15 21:54:53 +0900

    Update 두 큐 합 같게 만들기.py

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,18 +1,9 @@
-# 두 큐의 합이 같도록
-# 하나의 큐 골라 추출, 추출한 원소 다른 큐에
-
-# 필요한 작업으 최소 횟수 구하기
-# 모든 값의 합 구해서 나누기 2 (절반)
-# 절반보다 크면 pop, insert / 비교
-
 from collections import deque
 
 def solution(queue1, queue2):
     answer = 0
     queue1 = deque(queue1)
     queue2 = deque(queue2)
-    total = sum(queue1) + sum(queue2)
-    half = int(total/2)
     length = len(queue1)
     sum1 = sum(queue1)
     sum2 = sum(queue2)
