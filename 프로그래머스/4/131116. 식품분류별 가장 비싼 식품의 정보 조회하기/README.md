7671e48e3c723945eabbaf742dc0500ee254ae60
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-18 13:55:36 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/README.md
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 식품분류별 가장 비싼 식품의 정보 조회하기 - 131116 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131116#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131116) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2023년 10월 2일 23:7:26
+2023년 10월 3일 13:55:34
 
 ### 문제 설명
 
--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -9,7 +9,13 @@
 # GROUP BY CATEGORY
 # ORDER BY MAX_PRICE DESC
 
-SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
-FROM FOOD_PRODUCT
-WHERE (CATEGORY, PRICE) IN (SELECT CATEGORY, MAX(PRICE) FROM FOOD_PRODUCT WHERE CATEGORY IN ('과자', '국', '김치', '식용유') GROUP BY CATEGORY)
-ORDER BY MAX_PRICE DESC
+# SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
+# FROM FOOD_PRODUCT
+# WHERE (CATEGORY, PRICE) IN (SELECT CATEGORY, MAX(PRICE) FROM FOOD_PRODUCT WHERE CATEGORY IN ('과자', '국', '김치', '식용유') GROUP BY CATEGORY)
+# ORDER BY MAX_PRICE DESC
+
+select category, price as max_price, product_name
+from (select *, rank() over (partition by category order by price desc) as rk from food_product) as fp
+where fp.rk = 1 and category in ('과자', '국', '김치', '식용유')
+order by max_price desc
+
