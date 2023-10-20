a80d27e217a7be7a39fdfd435cd510d2e0e25bc7
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-20 23:34:20 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,9 +1,5 @@
-# 만원 단위의 가격대 별로 상품 개수를 출력
-# 가격대 오름차순 정렬
-
 select (case when price < 10000 then 0
-        else truncate(price, -4)
-        end) as price_group, count(product_id) as products
-from product 
+       else truncate(price, -4) end) as price_group, count(*) as products
+from product
 group by price_group
 order by price_group
