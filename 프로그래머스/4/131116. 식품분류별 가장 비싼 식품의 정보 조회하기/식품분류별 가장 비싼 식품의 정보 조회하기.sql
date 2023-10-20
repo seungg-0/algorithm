1332c26c707b8a27601cc30b3b13fe36d1b23272
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-20 23:47:22 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,21 +1,5 @@
-# 가격 젤 비싼 식품 조회
-# '과자', '국', '김치', '식용유' 만 추력
-# 식품 가격 기준으로 내림차순 정렬
-
-# 오답
-# SELECT CATEGORY, MAX(PRICE) AS MAX_PRICE, PRODUCT_NAME
-# FROM FOOD_PRODUCT
-# WHERE CATEGORY IN ('과자', '국', '김치', '식용유')
-# GROUP BY CATEGORY
-# ORDER BY MAX_PRICE DESC
-
-# SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
-# FROM FOOD_PRODUCT
-# WHERE (CATEGORY, PRICE) IN (SELECT CATEGORY, MAX(PRICE) FROM FOOD_PRODUCT WHERE CATEGORY IN ('과자', '국', '김치', '식용유') GROUP BY CATEGORY)
-# ORDER BY MAX_PRICE DESC
-
 select category, price as max_price, product_name
-from (select *, rank() over (partition by category order by price desc) as rk from food_product) as fp
-where fp.rk = 1 and category in ('과자', '국', '김치', '식용유')
-order by max_price desc
+from (select *, rank() over(partition by category order by price desc) as rk from food_product) as fp
+where category in ('과자', '국', '김치', '식용유') and rk = 1
+order by price desc
 
