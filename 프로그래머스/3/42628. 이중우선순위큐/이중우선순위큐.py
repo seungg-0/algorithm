c6954ac8778afd896e69f6a7cbc102de2cb23fc4
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-28 00:35:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,29 +1,29 @@
-from heapq import heapify, heappop, heappush, nlargest
+# I 숫자	큐에 주어진 숫자를 삽입합니다.
+# D 1	큐에서 최댓값을 삭제합니다.
+# D -1	큐에서 최솟값을 삭제합니다.
 
+from heapq import heapify, nlargest, heappop, heappush
 def solution(operations):
     answer = []
     queue = []
     heapify(queue)
+    # heappush(queue, 1)
+    # print(heappop(queue))
+
     for operation in operations:
-        left, right = operation.split(" ")
-        if left == 'I': # 큐에 주어진 숫자 삽입
-            heappush(queue, int(right))
-        elif right == '1': # 큐에서 최댓값을 삭제
-            if len(queue) == 0:
-                continue
-            if len(queue) == 1:
-                heappop(queue)
-            else:
-                # 최대 힙 만들어주기
-                queue = nlargest(len(queue), queue)[1:]
-                heapify(queue)
-        else: # 큐에서 최솟값을 삭제
-            if len(queue) >= 1: # 최솟값 삭제
+        typ, num = operation.split(" ")
+        if typ == 'I': # 큐에 주어진 숫자를 삽입합니다.
+            heappush(queue, int(num))
+        elif num == '1':
+            queue = nlargest(len(queue), queue)[1:]
+            heapify(queue)
+        else:
+            if len(queue) >= 1:
                 heappop(queue)
     
     if len(queue) == 0:
-        answer = [0, 0]
-    else:
-        answer.append(max(queue))
-        answer.append(queue[0])
+        return [0, 0]
+    answer.append(max(queue))
+    answer.append(heappop(queue))
+
     return answer
