e14fd7e172514a52c11ee6fb11be59d51de10666
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-27 21:38:14 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,14 +1,34 @@
+# def solution(n, costs):
+#     answer = 0
+#     costs.sort(key=lambda x:x[2]) # 가격 기준 오름차순 정렬
+#     link = set([costs[0][0]])
+    
+#     while len(link) < n:
+#         for node1, node2, cost in costs:
+#             if node1 in link and node2 in link: # 둘 다 이미 연결되어 있다면
+#                 continue
+#             if node1 in link or node2 in link: # 둘 중 하나는 연결되어 있지 않고 하나는 연결되어 있다면
+#                 link.update([node1], [node2])
+#                 answer += cost
+#                 break
+#     return answer
+
+# 최소의 비용으로 모든 섬이 서로 통행 가능하도록 필요한 최소 비용 리턴
+
 def solution(n, costs):
     answer = 0
-    costs.sort(key=lambda x:x[2]) # 가격 기준 오름차순 정렬
-    link = set([costs[0][0]])
+    costs = sorted(costs, key = lambda x:x[2])
+    connected = [0]*n
+    connected[costs[0][0]] = 1
     
-    while len(link) < n:
+    while sum(connected) < n:
         for node1, node2, cost in costs:
-            if node1 in link and node2 in link: # 둘 다 이미 연결되어 있다면
+            if connected[node1] and connected[node2]: # 둘 다 연결
                 continue
-            if node1 in link or node2 in link: # 둘 중 하나는 연결되어 있지 않고 하나는 연결되어 있다면
-                link.update([node1], [node2])
+            elif connected[node1] or connected[node2]:
+                connected[node1] = 1
+                connected[node2] = 1
                 answer += cost
                 break
+
     return answer
