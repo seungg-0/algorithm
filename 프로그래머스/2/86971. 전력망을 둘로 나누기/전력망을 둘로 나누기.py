493838d809fa1147935a5fcf166259a07c2a29d0
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-10-28 00:01:44 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,32 +1,33 @@
 from collections import deque
-
-def bfs(start, visited, connected):
-    queue = deque()
-    queue.append(start)
-    visited[0] = True
-    cnt = 1
-    while queue:
-        node = queue.popleft()
-        for w in connected[node]:
-            if not visited[w]:
-                visited[w] = True
-                queue.append(w)
-                cnt += 1
-    return cnt
-
 def solution(n, wires):
-    answer = 1e9        
+    answer = []
+    
+    def bfs(queue, visited):
+        cnt = 1
+        while queue:
+            n = queue.popleft()
+            for i in connected[n]:
+                if not visited[i]:
+                    visited[i] = True
+                    queue.append(i)
+                    cnt += 1
+        return cnt
+    
     for i in range(n-1):
-        visited = [False]*n
-        connected = [[] for _ in range(n)]
-        for idx, wire in enumerate(wires):
+        connected = [[] for _ in range(n+1)]
+        visited = [False]*(n+1)
+        # 연결 정보 저장
+        for idx, val in enumerate(wires):
             if idx == i:
                 continue
-            else: # 연결 정보 저장
-                connected[wire[0]-1].append(wire[1]-1)
-                connected[wire[1]-1].append(wire[0]-1)
-        
-        cnt = bfs(0, visited, connected)
-        answer = min(answer, abs((n-cnt)-cnt))
-                       
-    return answer
+            else:
+                connected[val[0]].append(val[1])
+                connected[val[1]].append(val[0])
+        # 연결 갯수 세기
+        queue = deque()
+        queue.append(1)
+        visited[1] = True
+        cnt = bfs(queue, visited)
+        answer.append(abs((n-cnt)-cnt))
+            
+    return min(answer)
