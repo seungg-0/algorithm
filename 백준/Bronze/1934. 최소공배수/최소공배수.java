e0f30c57045aa4fb3cab0123675c8836e00b7244
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2024-12-21 21:46:41 +0900

    Update 최소공배수.java

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for(int i = 0 ; i< t ; i++){
-            int a = sc.nextInt();
-            int b= sc.nextInt();
-            int result = a*b/gcd(a,b);
-            System.out.println(result);
-
-        }
+public class Main{
+    // 유클리드 호제법 (최대공약수 구하기)
+    public static int gcd(int a, int b){
+        if(b == 0) return a;
+        else return gcd(b, a%b);
     }
-
-    public static int gcd(int a , int b){
-        if(b==0)
-            return a;
-        else
-            //재귀 함수의 형태로 구현
-        return gcd(b, a%b);
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // 테스트 케이스 입력
+        int T = sc.nextInt();
+        
+        for(int i = 0 ; i<T ; i++){
+            // A, B 입력
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            // 최소공배수 : (num1 * num2) / 최대공약수
+            int result = A*B/gcd(A, B);
+            System.out.println(result);
+        }
+        sc.close();
     }
 }
