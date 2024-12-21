759d295734aacd37f1ada873f23ee4cded6321be
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2024-12-21 21:02:23 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14244 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -9,9 +9,13 @@ public class Main{
         char[] cNum = sNum.toCharArray();
         
         long sum = 0L;
+        long M = 1234567891;
+        long pow = 1L;
         for(int i=0; i<L; i++){
-            sum += (cNum[i]-'a'+1) * Math.pow(31, i);
+            sum += (cNum[i]-'a'+1)*pow;
+            // pow는 31를 매번 곱해주고, 곱해줄떄마다 1234567891를 나눠주면 long을 넘지 않음. 
+            pow = (pow * 31)%1234567891; 
         }
-        System.out.println(sum);
+        System.out.println(sum%M);
     }
 }
