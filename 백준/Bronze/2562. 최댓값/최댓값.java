3b6c777b612e4a1e906034ffa028d5b6e6fb40a1
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2024-12-22 22:29:29 +0900

    [Bronze III] Title: 최댓값, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] a = new int[10];
         int max = 0;
         int idx = 0;
-        for (int i=1; i<10; i++){
-            a[i] = Integer.parseInt(sc.nextLine());
-            if (a[i] > max){
-                max = a[i];
+        
+        for(int i=0; i<9; i++){
+            int input = sc.nextInt();
+            if(input > max){
+                max = input;
                 idx = i;
             }
         }
+        
         System.out.println(max);
-        System.out.print(idx);
+        System.out.println(idx+1);
+        
     }
 }
