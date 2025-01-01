51285d3c53fa4349630a2b9fe44e54b1e694915b
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2025-01-01 15:43:55 +0900

    [Bronze III] Title: 학점계산, Time: 164 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String score = sc.next();
+        
+        if(score.equals("A+")) System.out.println(4.3);
+        if(score.equals("A0")) System.out.println(4.0);
+        if(score.equals("A-")) System.out.println(3.7);
+        if(score.equals("B+")) System.out.println(3.3);
+        if(score.equals("B0")) System.out.println(3.0);
+        if(score.equals("B-")) System.out.println(2.7);
+        if(score.equals("C+")) System.out.println(2.3);
+        if(score.equals("C0")) System.out.println(2.0);
+        if(score.equals("C-")) System.out.println(1.7);
+        if(score.equals("D+")) System.out.println(1.3);
+        if(score.equals("D0")) System.out.println(1.0);
+        if(score.equals("D-")) System.out.println(0.7);
+        if(score.equals("F")) System.out.println(0.0);
+    }
+}
