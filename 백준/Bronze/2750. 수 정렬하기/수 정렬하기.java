f8d783d7879e9abc6825351ab7177afe5e834d5d
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2025-01-01 15:18:31 +0900

    [Bronze II] Title: 수 정렬하기, Time: 288 ms, Memory: 22468 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,18 +1,29 @@
-import java.util.*;
+//버블정렬을 통해 작성해본다.
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
         int N = sc.nextInt();
-        int[] arrays = new int[N];
+        int [] A = new int [N];
         
-        for(int i=0; i<N; i++){
-            arrays[i] = sc.nextInt();
+        //숫자 목록
+        for(int i = 0 ; i < N ; i++){
+            A[i] = sc.nextInt();
         }
-        Arrays.sort(arrays);
-        for(int i=0; i<N; i++){
-            System.out.println(arrays[i]);
+        
+        //현재 A 배열 갑소가 한칸 오른쪽 배열의 값 비교
+        for(int i = 0; i < N-1 ; i++){
+            for(int j = 0; j < N-1-i ; j++){
+                if(A[j]>A[j+1]){
+                    int temp = A[j];
+                    A[j]=A[j+1];
+                    A[j+1]=temp;
+                }
+            }
+        }
+        for(int i = 0; i<N ; i ++){
+            System.out.println(A[i]);
         }
     }
 }
