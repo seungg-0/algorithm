24e5f8c831c36ba7ed1e7acf6ccd6d56df6d62a7
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2025-02-08 14:57:34 +0900

    [Silver IV] Title: 수 찾기, Time: 580 ms, Memory: 50256 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,43 +1,28 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N];
-        for(int i = 0 ; i<N ; i++){
-            A[i] = sc.nextInt();
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
+        int N = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> Amap = new HashMap<>();
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++){
+            Amap.put(Integer.parseInt(st.nextToken()), 1); 
         }
-        //배열 정렬
-        Arrays.sort(A);
-
-        int M = sc.nextInt();
-        for(int i = 0 ; i<M ; i++){
-            boolean find = false;
-            int target = sc.nextInt();
-
-            //이진 탐색 시작
-            int start = 0 ;
-            int end = A.length-1;
-            while(start <= end){
-                int midi=(start+end)/2;
-                int midV = A[midi];
-
-                if(midV > target) {
-                    end = midi - 1;
-                }else if(midV < target){
-                    start = midi+1;
-                }else{
-                    find = true;
-                    break;
-                }
-            }
-            if(find){
-                System.out.println(1);
-            }else{
-                System.out.println(0);
+        
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<M; i++){
+            if(Amap.containsKey(Integer.parseInt(st.nextToken()))){
+                sb.append(1).append("\n");
+            } else{
+                sb.append(0).append("\n");
             }
         }
+        System.out.println(sb);
     }
 }
