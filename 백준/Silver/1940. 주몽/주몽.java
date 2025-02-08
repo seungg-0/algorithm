8f4c1a1f95f67fa04b2200c5c9d179c52828cae2
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2025-02-08 14:41:46 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16500 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,49 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //재료의 개수
-        int N = Integer.parseInt(br.readLine());
-        //갑옷을 만드는 데 필요한 수
-        int M = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        //재료들의 고유한 번호
-        int [] nums = new int[N];
-        for(int i = 0; i < N; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = null;
+        
+        long N = Long.parseLong(br.readLine());
+        long M = Long.parseLong(br.readLine());
+        
+        int size = (int) N;
+        long[] materials = new long[size]; // [암기] 배열을 지정하는 값은 반드시 int타입이어야 한다 !
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<size; i++){
+            materials[i] = Long.parseLong(st.nextToken());
         }
-
-        //시간 단축을 위해 값을 정렬한다.
-        Arrays.sort(nums);
-
-        //투포인터를 활용하여 번호의 합을 구한다.
-        /*
-        자꾸 배열을 단순 반복하여 활용하려는 습관이 있다.
-        고치도록하자.
-         */
-        int start = 0;
-        int end = N-1;
-
-        //경우의 수
-        int count = 0;
-        while(start < end){
-            if(nums[start] + nums[end] < M){
-                start++;
-            }else if(nums[start] + nums[end] > M){
-                end--;
-            }else{
-                count++;
-                start++;
-                end--;
+        
+        Arrays.sort(materials); // [복습] 오름차순 정렬
+        
+        if(N==1){
+            System.out.println(0);
+        } else{
+            long cnt = 0;
+            int s = 0;
+            int e = size-1;
+            while(s<e){
+                if((materials[s]+materials[e])==M){
+                    cnt++;
+                    s++;
+                    e--;
+                } else if((materials[s]+materials[e])<M){
+                    s++;
+                } else{
+                    e--;
+                }
             }
-        }
-        System.out.println(count);
-        br.close();
+            System.out.println(cnt);  
+        }  
     }
 }
