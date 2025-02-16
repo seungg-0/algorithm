0353ce2abbd2a80d265d36d59f207b6c76cb6e0d
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2025-02-16 19:22:30 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9296 ms, Memory: 305560 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,18 +1,20 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m; // n개 컴퓨터, m개 신뢰관계
-    static List<Integer>[] lists; // 컴퓨터 신뢰 관계 (인접 리스트)
-    static int[] hackCounts; // [i]번 컴퓨터를 해킹 했을 떄, 해킹 가능한 컴퓨터 수
+    static int n, m;					// n개 컴퓨터, m개 컴퓨타 신뢰 관계
+    static List<Integer>[] lists;		// 컴퓨터 신뢰 관계 (인접 리스트)
+    static int[] hackCounts;			// [i]번 컴퓨터를 해킹 했을때, 해킹 가능한 컴퓨터 수
     static boolean[] visited;
     static Queue<Integer> queue = new LinkedList<>();
-    
-    static void bfs(){
-        while(!queue.isEmpty()){
+
+    static void bfs() {
+        while (!queue.isEmpty()) {
             int currentIdx = queue.remove();
-            for(int nextIdx : lists[currentIdx]){
-                if(!visited[nextIdx]){
+
+            for (int nextIdx : lists[currentIdx]) {
+                if (!visited[nextIdx]) {
                     visited[nextIdx] = true;
                     hackCounts[nextIdx]++;
                     queue.add(nextIdx);
@@ -20,47 +22,49 @@ public class Main {
             }
         }
     }
-    
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in)
+        );
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        
-        lists = new List[n+1]; // 컴퓨터 번호 1~n
-        hackCounts = new int[n+1];
-        for(int i=1; i<=n; i++){
+
+        lists = new List[n + 1];			// 컴퓨터(정점) 번호 [1] ~ [n] 사용
+        hackCounts = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             lists[i] = new ArrayList<>();
         }
-        
-        for(int i=0; i<m; i++){
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            
-            lists[start].add(end); // start가 end를 신뢰한다
+
+            lists[start].add(end);
         }
-        
-        // 각 컴퓨터를 시작 노드로 하여 BFS탐색 수행
-        for(int i=1; i<=n; i++){
-            visited = new boolean[n+1]; // 방문 처리 배열 초기화
-            
+
+        // 각 [i]번 컴퓨터를 시작 노드로 하여 BFS 탐색 수행
+        for (int i = 1; i <= n; i++) {
+            visited = new boolean[n + 1];	// 방문 처리 배열 초기화
+
             visited[i] = true;
             hackCounts[i]++;
             queue.add(i);
-            
+
             bfs();
         }
-        
+
         int maxHackCount = 0;
-        for(int i=1; i<=n; i++){
-            maxHackCount = Math.max(maxHackCount, hackCounts[i]); // [암기]Math.max(a, b)
+        for (int i = 1; i <= n; i++) {
+            maxHackCount = Math.max(maxHackCount, hackCounts[i]);
         }
-        
+
         StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=n; i++){
-            if(hackCounts[i] == maxHackCount){
+        for (int i = 1; i <= n; i++) {
+            if (hackCounts[i] == maxHackCount) {
                 sb.append(i).append(" ");
             }
         }
