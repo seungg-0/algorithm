da868ee1a74784127dcfd1a4657d049e06574b21
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2025-08-12 21:32:00 +0900

    [Gold IV] Title: 거짓말, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -2,88 +2,86 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
+    static int N, M;
+    static boolean[] isKnowPerson;
     static int[] par;
-    static boolean[] isknowPerson;
-    
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken()); // num of people
-        int M = Integer.parseInt(st.nextToken()); // num of party
-        st = new StringTokenizer(br.readLine());
-        int knows = Integer.parseInt(st.nextToken()); // num of people who know that is false
         
-        par = new int[N+1]; // 대표 노드 저장
-        for(int i=0; i<=N; i++){
-            par[i] = i; // 초기 루트노드는 자기자신
-        }
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         
-        int result = 0;
-        
-        isknowPerson = new boolean[N+1]; // 진실을 아는 사람들
+        st = new StringTokenizer(br.readLine(), " "); // 거짓말 아는 사람 수, 아는 사람 번호들 입력
+        int knows = Integer.parseInt(st.nextToken());
+        isKnowPerson = new boolean[N+1]; // [주의] 인덱스 에러 항상 유의 !! 
         ArrayList<ArrayList<Integer>> party = new ArrayList<>(); // 같은 파티에 참여하는 사람들
         for(int i=0; i<M; i++){
             party.add(new ArrayList<>());
         }
         
-        if(knows==0){
-            System.out.println(M);
-            System.exit(0);
-        } else{
-            for(int i=0; i<knows; i++){
-                isknowPerson[Integer.parseInt(st.nextToken())] = true;
-            }
-            
-            for(int i=0; i<M; i++){ // 파티별 탐색 
-                st = new StringTokenizer(br.readLine());
-                int n = Integer.parseInt(st.nextToken()); // 파티 참가인원
-                int first = Integer.parseInt(st.nextToken()); // 파티에 참여하는 첫번째 사람
-                party.get(i).add(first); // 파티에 저장 
-                
-                for(int j = 1; j<n; j++){
-                    int next = Integer.parseInt(st.nextToken());
-
-                    union(first, next); // 첫 번째 사람을 기준으로 union
-                    party.get(i).add(next); // 참여인원들 파티에 저장
-                }
-            }
-            
-            for(int i=0; i<M; i++){
-                boolean check = true; // 거짓말 가능한 파티인가?
-                
-                for(int p : party.get(i)){
-                    if(isknowPerson[find(p)]){ // true : 진실을 아는 사람
-                        check = false;
-                        break; // 탐색종료
-                    }
-                }
-                if(check){
-                    result++; // 거짓말 칠 수 있는 파티 +1
-                }
-            }
-            System.out.println(result);
+        for(int i=0; i<knows; i++){ // 거짓말인지 아는 사람 정보 저장하기
+            isKnowPerson[Integer.parseInt(st.nextToken())] = true;
         }
+        
+        par = new int[N+1]; // [주의] 인덱스 에러 항상 유의 !! 
+        for(int i=0; i<=N; i++){ // 부모 노드 초기값 저장
+            par[i] = i; // 초기 부모는 자기자신
+        }
+        
+        int participants;
+        int result = 0;
+        for(int i=0; i<M; i++){ // 파티에 정보 입력받기 
+            st = new StringTokenizer(br.readLine(), " ");
+            participants = Integer.parseInt(st.nextToken()); // 해당 파티 참여자 수 입력받기
+            int num1, num2, tmp;
+            num1 = Integer.parseInt(st.nextToken());
+            party.get(i).add(num1);
+            for(int j=1; j<participants; j++){
+                num2 = Integer.parseInt(st.nextToken());
+                party.get(i).add(num2);
+                union(num1, num2);
+                num1 = num2;
+            }
+        }
+        
+        for(int i=0; i<M; i++){
+            boolean flag = true;
+            for(int p : party.get(i)){
+                if(isKnowPerson[find(p)]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                result++;
+            }
+        }
+        
+        System.out.println(result);
+        
     }
     
-    public static int find(int x){ // 루트 노드 찾기
-        if(par[x] == x){
-            return x;
+    public static int find(int a){
+        if(par[a]==a){
+            return a;
         } else{
-            return find(par[x]);
+            return find(par[a]);
         }
     }
+        
     
     public static void union(int a, int b){ // 합치기
-        // 두 사람이 참여한 파티의 루트노드
-        int pa = find(a);
-        int pb = find(b);
-        
-        if(isknowPerson[pb]){ // b쪽 루트가 진실을 알고 있다면
-            int tmp = pa;
-            pa = pb;
-            pb = tmp;
-        } // 둘이 자리 바꾸기
-        
-        par[pb] = pa; // 루트 노드 등록
-    }
+        // 부모 노드 찾기
+        int aPar = find(a);
+        int bPar = find(b);
+        if(isKnowPerson[bPar]){ // b쪽 루트가 진실을 알고있으면, a에 b를 붙이는데, 이 경우는 a를 b에 붙여야하므로 값 교환
+            int tmp = aPar;
+            aPar = bPar;
+            bPar = tmp; 
+        }
+        par[bPar] = aPar; // b쪽 루트가 진실을 아는 경우 aPar 의 루트값을 바꿔주게 됨
+ 
+    } 
 }
