2e51b7664182dde289a462f2cf953fd2e1e72676
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2026-02-17 14:11:45 +0900

    [Gold IV] Title: 알파벳, Time: 796 ms, Memory: 15156 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,61 +1,58 @@
-// 알파벳 각각 한 번씩만 지나갈 수 있음 -> 알파벳으로 방문처리
-import java.io.*;
+// 동일한 알파벳 지날 수 없음 
+// 말이 최대한 이동할 수 있는 거리 구하기 (시작한 칸 포함)
+// 백트래킹 ! 방문 처리를 알파벳으로
+// 즉, 여기서도 지날 수 있는지 확인하고 넣기. 
+
 import java.util.*;
+import java.io.*;
 
 public class Main{
-    static int[] dx, dy;
     static int R, C;
     static int[][] map;
+    static int result;
     static boolean[] visited;
-    static int x, y;
-    static int maxCount = 1;
-
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
         
-        dx = new int[] {0, 0, -1, 1};
-        dy = new int[] {-1, 1, 0, 0};
+        st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
+        visited = new boolean[27];
         map = new int[R][C];
-        visited = new boolean[26]; // 0~16 : A~Z
-        
-        // int num = ch - 'A' + 1;
-        
-        String str = "";
-        int j;
         for(int i=0; i<R; i++){
-            str = br.readLine();
-            j = 0;
-            for(char c : str.toCharArray()){ // [암기]
-                map[i][j] = c - 'A'; // A 이면 0, B이면 1, .. 
-                j++;
+            String tmp = br.readLine();
+            char c;
+            for(int j=0; j<C; j++){
+                c = tmp.charAt(j);
+                map[i][j] = c - 'A' + 1; // [암기 !!!]
             }
         }
-        
-        // System.out.println(Arrays.deepToString(map));
-        // System.out.println(Arrays.toString(visited));
-        // BFS가 아닌 DFS로 풀어야 함 (우우우우/하하하하 로 이동할 수 있기 때문. 이동할 수 있는 "최대"거리를 구하는 문제)
+        result = 1;
         visited[map[0][0]] = true;
-        DFS(0, 0, 1);
-        System.out.println(maxCount);
-        
+        DFS(1, 0, 0);
+        // result = Math.max(result,DFS(1));
+        System.out.println(result);
     }
     
-    static void DFS(int x, int y, int count){
-        maxCount = Math.max(maxCount, count); // 현재까지 최대 이동거리 
+    static void DFS(int cnt, int x, int y){
+        // 종료 조건
+        if(cnt > result){
+            result = cnt;
+        }
         
+        int nx, ny;
         for(int i=0; i<4; i++){
-            int nx = x+dx[i]; // 전역변수로 선언하면 재귀간 충돌 일어남 
-            int ny = y+dy[i]; // 서로 다른 재귀 호출이 같은 변수(nx, ny)를 공유하기 때문에 [중요/암기]
-            if(nx>=0 && nx<R && ny>=0 && ny<C){
-                if(!visited[map[nx][ny]]){
+            nx = x+dx[i];
+            ny = y+dy[i];
+            if(0<=nx && nx < R && 0<=ny && ny < C && !visited[map[nx][ny]]){
                 visited[map[nx][ny]] = true;
-                DFS(nx, ny, count + 1);
+                DFS(cnt+1, nx, ny);
                 visited[map[nx][ny]] = false;
-                }
             }
         }
     }
 }
+
