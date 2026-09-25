63ec58a1f413fadc44412e4e89306059d2bc57d1
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2026-09-25 16:47:18 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.10 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,17 +1,12 @@
-# 1~n
-# 파기해야 할 개인정보 번호들 구하기 
-# 모든 달 28일까지 있음 
-
 def solution(today, terms, privacies):
     answer = []
     
+    # 딕셔너리 선언 변수 = {}
     term_dict = {}
     
-
     tyear, tmonth, tday = map(int, today.split("."))
-
     
-     # 약관 정보 dict 로 저장
+    # 약관 정보 dict 로 저장
     for term in terms:
         k, v = term.split(" ")
         term_dict[k] = int(v)
@@ -24,21 +19,17 @@ def solution(today, terms, privacies):
         ms = term_dict[key]
         cy = ms//12
         cm = ms%12
-        print(term_dict[key])
-        print(cy, cm, year, month, day)
         
+        # 문제 잘 읽기
         if((month+cm)>12):
             year += (cy+1)
             month = month+cm-12
         else:
             year += cy
             month += cm
-            
-
-        print(tyear, tmonth, tday, year, month, day)
-        print(tyear*10000+tmonth*100+tday, year*10000+month*100+day)
+        
+        # 코드 작성 전에 인덱스 및 대소비교 확실하게 검증하고 짜기
         if (tyear*10000+tmonth*100+tday>=year*10000+month*100+day):
             answer.append(idx+1)
-
     
     return answer
