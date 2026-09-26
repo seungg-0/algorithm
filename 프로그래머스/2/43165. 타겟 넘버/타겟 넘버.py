73206e7aaab17d1b1cd32364b45386a4f5bd6b92
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2026-09-26 15:34:16 +0900

    [level 2] Title: 타겟 넘버, Time: 115.69 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,24 @@
+'''
+numbers (사용할 수 있는 숫자들 담긴)
+타겟 숫자 만드는 방법 수 return
+'''
+
 def solution(numbers, target):
     answer = 0
-    n = len(numbers)
     
-    def dfs(idx, num):
-        if n == idx:
-            if num == target:
-                nonlocal answer
+    def dfs(idx, total):
+        nonlocal answer # [암기] nonlocal
+        # 종료조건
+        if(idx==len(numbers)):
+            if(total==target):
                 answer += 1
             return
         
-        dfs(idx+1, num+numbers[idx])
-        dfs(idx+1, num-numbers[idx])
+        # 두 갈래로 재귀 호출 해야 함 (두갈래 호출 아래와 같이)
+        # 숫자를 더하는 경우
+        dfs(idx+1, total+numbers[idx])
+        # 숫자를 빼는 경우
+        dfs(idx+1, total-numbers[idx])
         
     dfs(0, 0)
-    
     return answer
-
-
